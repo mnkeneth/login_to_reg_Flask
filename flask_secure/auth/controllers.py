63fef87d8db554873876e2b901d12ae3c49e4fbd
Keneth M <mkn.keneth@gmail.com>
2021-10-09 20:37:28 +0300

    adding code block for user registration when user is logged in with Admin privileges.

--- flask_secure/auth/controllers.py
+++ flask_secure/auth/controllers.py
@@ -1,8 +1,15 @@
 #App Modules.
-from flask_secure import app
+from flask_secure import app, db
+from flask_secure.auth.forms import ExtendedRegisterForm
+from flask_secure.auth.models import User
 
 # System Modules
-from flask_security import LoginForm, url_for_security, RegisterForm
+from flask_security import LoginForm, url_for_security, \
+                            roles_accepted, registerable, roles_required
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+
+
+create = Blueprint('create', __name__, url_prefix='/create')
 
 # Log in Parameter for Flask_Security
 @app.context_processor
@@ -11,10 +18,35 @@ def login_context():
         'url_for_security': url_for_security,
         'login_user_form': LoginForm(),
     }
+
 # Registeration Form for Flask_Security
-@app.context_processor
-def register_context():
-    return {
-        'url_for_security': url_for_security,
-        'register_user_form': RegisterForm(),
-    }
+# To-Do: Define url to be only accessible by the admin users.
+@create.route('/user', methods=['GET', 'POST'])
+@roles_required("admin")
+def create_user():
+    form = ExtendedRegisterForm(request.form)
+    if request.method == 'GET':
+        return render_template('security/register_user.html', 
+                            form=form)
+                            
+    elif request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        user_exists = db.session.query(User).filter_by(email=email).first()
+        if user_exists == True:
+            form.email.errors.append(email + ' is already associated with another user')
+            form.email.data = str(email)
+            return render_template('security/register_user.html', 
+                                    form=form)
+
+        else:
+            registerable.register_user(
+                username = username,
+                email = email,
+                password=password
+                )
+            return redirect(url_for('home'))
+    
+    return render_template('security/register_user.html', 
+                            form=form)
